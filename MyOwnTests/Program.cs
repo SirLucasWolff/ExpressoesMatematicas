a782ccbc145d23fd87c7d7b92d2ab720444a3f19
SirLucasWolff <lucaswolff2014@hotmail.com>
2021-10-29 14:05:55 -0300

    Add project files.

---
+++ MyOwnTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
