c1fef8e10e9134326d6129096cec20e3c4522a2e
johnboeira <jbiel72001@gmail.com>
2021-10-29 14:27:19 -0300

    testes

---
+++ ExpressoesMatematicas.UnitTest/CalculadoraTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ExpressoesMatematicas.UnitTest
+{
+    [TestClass]
+    public class CalculadoraTests
+    {
+        [TestMethod]
+        public void deveCalcularSomaEmParenteses()
+        {
+            //arrange
+            string valoresParaCalcular = "1 + ( 1 + 1 )";
+            //act
+            
+            //assert
+        }
+    }
+}
